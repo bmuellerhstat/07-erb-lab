e389d87c1482be7fb8b896975ec2833e937428ec
kathleens0782 <Kathleens0782@hstat.org>
2018-02-08 08:18:13 +0000

    got 3/4 test to pass

--- app.rb
+++ app.rb
@@ -4,6 +4,15 @@ class App < Sinatra::Base
     get '/' do
         erb :index
     end
-
-
+    
+#     <!DOCTYPE html>
+# <html>
+#   <head>
+#     <meta charset="UTF-8">
+#     <title>title</title>
+#   </head>
+#   <body>
+  
+#   </body>
+# </html>
 end
